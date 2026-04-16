d31221dba1ed074b1fb165e4f6b81a65e50e5102
m-singhal <res.mridul@gmail.com>
2026-04-16 18:51:10 +1000

    Same as last commit with minor fixes.

--- README.md
+++ README.md
@@ -228,6 +228,25 @@ When exporting results, the HDF5 file preserves the internal dictionary structur
 
 ### 📥 Step 1: Load file
 ```python
+import h5py
+import numpy as np
+
+def load_hdf5_results(filename):
+    def load_group(group):
+        data = {}
+        for key in group.keys():
+            item = group[key]
+
+            if isinstance(item, h5py.Group):
+                data[key] = load_group(item)
+            else:
+                data[key] = item[()]
+
+        return data
+
+    with h5py.File(filename, "r") as f:
+        return load_group(f)
+        
 data = load_hdf5_results("spectra_output.h5")
 ```
 
--- app.py
+++ app.py
@@ -359,6 +359,25 @@ if not run:
         st.markdown("### 📥 Step 1: Load file")
 
         st.code("""
+    import h5py
+    import numpy as np
+
+    def load_hdf5_results(filename):
+        def load_group(group):
+            data = {}
+            for key in group.keys():
+                item = group[key]
+
+                if isinstance(item, h5py.Group):
+                    data[key] = load_group(item)
+                else:
+                    data[key] = item[()]
+
+            return data
+
+        with h5py.File(filename, "r") as f:
+            return load_group(f)
+    
     data = load_hdf5_results("spectra_output.h5")
     """, language="python")
 
